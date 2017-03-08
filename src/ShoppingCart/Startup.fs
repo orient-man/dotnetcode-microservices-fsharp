5fbfd9c909f3f6b0e26e0f45e7a4838a2552908f
Marcin Malinowski <marcin.malinowski@mbank.pl>
2017-03-08 16:41:52 +0100

    ShoppingCart skeleton

---
+++ src/ShoppingCart/Startup.fs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingCart
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Owin
+open Nancy
+open Nancy.Configuration
+open Nancy.Owin
+
+type Startup () =
+    member __.Configure(app: IApplicationBuilder) =
+        app.UseOwin(fun buildFunc -> buildFunc.UseNancy() |> ignore) |> ignore
+
+type TracingBootstrapper () =
+    inherit Nancy.DefaultNancyBootstrapper ()
+    override __.Configure(env) =
+        env.Tracing(enabled = true, displayErrorTraces = true)
